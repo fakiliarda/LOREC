7a411e1bf26d5fb7a3e3cd8f20493db7025945b0
fakiliarda <ardafakili@gmail.com>
2019-02-22 21:43:15 +0300

    som

--- TEST1_voc/object_detection_custom.py
+++ TEST1_voc/object_detection_custom.py
@@ -20,7 +20,7 @@ from collections import defaultdict
 from aiy.vision.inference import ModelDescriptor, ThresholdingConfig, FromSparseTensorConfig
 from aiy.vision.models import utils
 
-_COMPUTE_GRAPH_NAME = 'preTrainedVocDataset.binaryproto'
+_COMPUTE_GRAPH_NAME = '/examples/vision/LOREC/LOREC/BinaryProtospreTrainedVocDataset.binaryproto'
 _MACHINE_EPS = sys.float_info.epsilon
 _SCORE_TENSOR_NAME = 'concat_1'
 _ANCHOR_TENSOR_NAME = 'concat'
@@ -51,27 +51,27 @@ def sparse_configs(threshold=_DEFAULT_THRESHOLD):
 
 class Object:
     """Object detection result."""
-	background = 0
-	aeroplane = 1
-	bicycle = 2
-	bird = 3
-	boat = 4
-	bottle = 5
-	bus = 6
-	car = 7
-	cat = 8
-	chair = 9
-	cow = 10
-	diningtable = 11
-	dog = 12
-	horse = 13
-	motorbike = 14
-	person = 15
-	pottedplant = 16
-	sheep = 17
-	sofa = 18
-	train = 19
-	tvmonitor = 20
+    background = 0
+    aeroplane = 1
+    bicycle = 2
+    bird = 3
+    boat = 4
+    bottle = 5
+    bus = 6
+    car = 7
+    cat = 8
+    chair = 9
+    cow = 10
+    diningtable = 11
+    dog = 12
+    horse = 13
+    motorbike = 14
+    person = 15
+    pottedplant = 16
+    sheep = 17
+    sofa = 18
+    train = 19
+    tvmonitor = 20
 
     _LABELS = {
 		background: 'background',
